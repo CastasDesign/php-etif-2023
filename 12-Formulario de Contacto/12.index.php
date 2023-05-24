e86f2a881de6df55d1985217021ee9cd6895a125
Dsen6301 <125303601+CastasDesign@users.noreply.github.com>
2023-05-24 17:06:16 +0200

    25/05/23

--- 12-Formulario de Contacto/12.index.php
+++ 12-Formulario de Contacto/12.index.php
@@ -3,47 +3,85 @@
 $errores = '';
 $enviado = '';
 
-if (isset($_POST['submit'])) {
-	$nombre = $_POST['nombre'];
-	$correo = $_POST['correo'];
-	$mensaje = $_POST['mensaje'];
+//Archivo completo que revisa el METHOD GET o POST
 
-	if (!empty($nombre)) {
-		$nombre = trim($nombre);
-		$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
-	} else {
-		$errores .= 'Por favor escribe un nombre <br />';
-	}
-
-	if (!empty($correo)) {
-		$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
-
-		if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
-			$errores .= 'Por favor escribe un correo valido <br />';
+$metodo= $_SERVER['REQUEST_METHOD']; //GET o POST?
+if ($metodo == 'POST') {//Enviado mediante POST
+    if (isset($_POST['submit'])) {
+		$nombre = $_POST['nombre'];
+		$correo = $_POST['correo'];
+		$mensaje = $_POST['mensaje'];
+	
+		if (!empty($nombre)) {
+			$nombre = trim($nombre);
+			$nombre = htmlspecialchars($nombre);
+			$nombre = stripslashes($nombre);
+			// $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+		} else {
+			$errores .= 'Por favor escribe un nombre <br />';
 		}
-	} else {
-		$errores .= 'Por favor escribe un correo <br />';
+	
+		if (!empty($correo)) {
+			$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+	
+			if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+				$errores .= 'Por favor escribe un correo valido <br />';
+			}
+		} else {
+			$errores .= 'Por favor escribe un correo <br />';
+		}
+	
+		if(!empty($mensaje)){
+			$mensaje = htmlspecialchars($mensaje);
+			$mensaje = trim($mensaje);
+			$mensaje = stripslashes($mensaje);
+		} else {
+			$errores .= 'Por favor escribe el mensaje <br />';
+		}
+	
+		if(!$errores){
+			$enviado = 'true';
+		}
+	
 	}
-
-	if(!empty($mensaje)){
-		$mensaje = htmlspecialchars($mensaje);
-		$mensaje = trim($mensaje);
-		$mensaje = stripslashes($mensaje);
-	} else {
-		$errores .= 'Por favor escribe el mensaje <br />';
+} else { //Enviado mediante GET
+    if (isset($_GET['submit'])) {
+		$nombre = $_GET['nombre'];
+		$correo = $_GET['correo'];
+		$mensaje = $_GET['mensaje'];
+	
+		if (!empty($nombre)) {
+			$nombre = trim($nombre);
+			$nombre = htmlspecialchars($nombre);
+			$nombre = stripslashes($nombre);
+			// $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+		} else {
+			$errores .= 'Por favor escribe un nombre <br />';
+		}
+	
+		if (!empty($correo)) {
+			$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+	
+			if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+				$errores .= 'Por favor escribe un correo valido <br />';
+			}
+		} else {
+			$errores .= 'Por favor escribe un correo <br />';
+		}
+	
+		if(!empty($mensaje)){
+			$mensaje = htmlspecialchars($mensaje);
+			$mensaje = trim($mensaje);
+			$mensaje = stripslashes($mensaje);
+		} else {
+			$errores .= 'Por favor escribe el mensaje <br />';
+		}
+	
+		if(!$errores){
+			$enviado = 'true';
+		}
+	
 	}
-
-	if(!$errores){
-		$enviar_a = 'tunombre@tuempresa.com';
-		$asunto = 'Correo enviado desde miPagina.com';
-		$mensaje_preparado = "De: $nombre \n";
-		$mensaje_preparado .= "Correo: $correo \n";
-		$mensaje_preparado .= "Mensaje: " . $mensaje;
-
-		//mail($enviar_a, $asunto, $mensaje_preparado);
-		$enviado = 'true';
-	}
-
 }
 
 require '12.index.view.php';
